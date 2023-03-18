922c5ed8c7a534fc65790c983cbe9334ef117f1f
Russel <russeljaime.monedero.m@bulsu.edu.ph>
2023-03-19 00:26:04 +0800

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,9 +4,12 @@ import { Observable } from 'rxjs';
 
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
+
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -15,6 +18,11 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
     .map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl)
+    .map((response) => <Product[]>response.json());
   }
 
 }
