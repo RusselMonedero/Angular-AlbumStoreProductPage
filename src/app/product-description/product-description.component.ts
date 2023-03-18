17d886716855c36fbca01f88293d8f56cf209fe5
Russel <russeljaime.monedero.m@bulsu.edu.ph>
2023-03-18 22:56:35 +0800

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor(_productService) { }
+  constructor(_productService: ProductService) { }
 
   ngOnInit() {
   }
